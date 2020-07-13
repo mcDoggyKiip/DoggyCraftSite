a92948e65801220fb74486a185b98d73f2cfa4eb
mcDoggyKiip <eijk0037@hz.nl>
2020-07-13 18:42:16 +0200

    make mc_username no longer required for the site to work
    Signed-off-by: mcDoggyKiip <eijk0037@hz.nl>

--- database/migrations/2020_07_12_122437_add_mc_username_to_users_table.php
+++ database/migrations/2020_07_12_122437_add_mc_username_to_users_table.php
@@ -14,7 +14,7 @@ class AddMcUsernameToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('mc_username');
+            $table->string('mc_username')->nullable();
         });
     }
 
