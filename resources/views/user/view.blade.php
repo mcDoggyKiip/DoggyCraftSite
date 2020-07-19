ef57127580f25a9acb3bea4853cc71ab891e4d12
mcDoggyKiip <eijk0037@hz.nl>
2020-07-19 18:32:20 +0200

    add comments to the view as requested in PR #3
    Signed-off-by: mcDoggyKiip <eijk0037@hz.nl>

--- resources/views/user/view.blade.php
+++ resources/views/user/view.blade.php
@@ -3,14 +3,17 @@
 @section('content')
     name: {{$user->name}}
     <br/>
+    {{-- display the info if the logged in user belongs to the owner group. requirers the logged in user to have mc_username set--}}
     @if(isset(Auth::user()->mc_username))
         @if(in_array("owner", Auth::user()->LpPlayer->groups()))
             email: {{$user->email}}
             <br/>
+            {{-- check if the requeste user has mc_username set--}}
             @if(isset($user->mc_username))
                 mc username: {{$user->mc_username}}
                 <br/>
                 permission group(s):
+                {{-- loop through all groups for the requested user--}}
                 @foreach($user->LpPlayer->groups() as $group)
                     @if(!$loop->last)
                         {{$group}},
@@ -21,12 +24,14 @@
                 <br/>
                 user permissions:
                 <br/>
+                {{-- loop through all permissions for the requested user--}}
                 @foreach($user->LpPlayer->LpUserPermission as $perm)
                     - {{$perm = $perm->permission}}
                 @endforeach
                 <br/>
                 group permissions:
                 <br/>
+                {{-- loop through all permissions for the groups of the requested user--}}
                 @foreach($user->LpPlayer->LpGroupPermission() as $permgroup)
                     @foreach($permgroup as $perm)
                         @if(!$loop->last)
