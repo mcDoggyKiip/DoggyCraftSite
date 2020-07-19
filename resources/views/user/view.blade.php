ffbcf46a307d140a39cce4b195088e259d0cfc7b
mcDoggyKiip <eijk0037@hz.nl>
2020-07-19 16:39:42 +0200

    hide potentially sensitive information from unauthorized people
    Signed-off-by: mcDoggyKiip <eijk0037@hz.nl>

--- resources/views/user/view.blade.php
+++ resources/views/user/view.blade.php
@@ -3,36 +3,40 @@
 @section('content')
     name: {{$user->name}}
     <br/>
-    email: {{$user->email}}
-    <br/>
-    @if(isset($user->mc_username))
-        mc username: {{$user->mc_username}}
-        <br/>
-        permission group(s):
-        @foreach($user->LpPlayer->groups() as $group)
-            @if(!$loop->last)
-                {{$group}},
-            @else
-                {{$group}}
-            @endif
-        @endforeach
-        <br/>
-        user permissions:
-        <br/>
-        @foreach($user->LpPlayer->LpUserPermission as $perm)
-            - {{$perm = $perm->permission}}
-        @endforeach
-        <br/>
-        group permissions:
-        <br/>
-        @foreach($user->LpPlayer->LpGroupPermission() as $permgroup)
-            @foreach($permgroup as $perm)
-                @if(!$loop->last)
-                    - {{$perm = $perm->permission}}<br>
-                @else
+    @if(isset(Auth::user()->mc_username))
+        @if(in_array("owner", Auth::user()->LpPlayer->groups()))
+            email: {{$user->email}}
+            <br/>
+            @if(isset($user->mc_username))
+                mc username: {{$user->mc_username}}
+                <br/>
+                permission group(s):
+                @foreach($user->LpPlayer->groups() as $group)
+                    @if(!$loop->last)
+                        {{$group}},
+                    @else
+                        {{$group}}
+                    @endif
+                @endforeach
+                <br/>
+                user permissions:
+                <br/>
+                @foreach($user->LpPlayer->LpUserPermission as $perm)
                     - {{$perm = $perm->permission}}
-                @endif
-            @endforeach
-        @endforeach
+                @endforeach
+                <br/>
+                group permissions:
+                <br/>
+                @foreach($user->LpPlayer->LpGroupPermission() as $permgroup)
+                    @foreach($permgroup as $perm)
+                        @if(!$loop->last)
+                            - {{$perm = $perm->permission}}<br>
+                        @else
+                            - {{$perm = $perm->permission}}
+                        @endif
+                    @endforeach
+                @endforeach
+            @endif
+        @endif
     @endif
 @endsection
