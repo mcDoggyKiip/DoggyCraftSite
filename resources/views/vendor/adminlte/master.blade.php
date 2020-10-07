2b062fc3c736e2a4aac54e3c0698dc16bbbc996b
mcDoggyKiip <eijk0037@hz.nl>
2020-10-07 19:06:42 +0200

    change to the official way to do the laravel mix things
    Signed-off-by: mcDoggyKiip <eijk0037@hz.nl>

--- resources/views/vendor/adminlte/master.blade.php
+++ resources/views/vendor/adminlte/master.blade.php
@@ -74,7 +74,6 @@
         <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
         <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
         <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script>
-        <script src="{{ asset('js/app.js') }}"></script>
 
         {{-- Configured Scripts --}}
         @include('adminlte::plugins', ['type' => 'js'])
